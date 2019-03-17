d6ed602dba4f92d46e08cc82d758b8cc8fa3df0d
CJ Moro <scjmoro@gmail.com>
2019-03-16 23:30:24 -0700

    Add stripe charge

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -2,17 +2,12 @@ const keys = require("../config/keys");
 const stripe = require("stripe")(keys.stripeSecretKey);
 
 module.exports = function billingRoutes(app) {
-  app.post("/api/stripe", (req, res) => {
-    stripe.charges.create(
-      {
-        amount: 2000,
-        currency: "cad",
-        source: "tok_visa", // obtained with Stripe.js
-        description: "Charge for jenny.rosen@example.com"
-      },
-      function(err, charge) {
-        // asynchronously called
-      }
-    );
+  app.post("/api/stripe", async (req, res) => {
+    const charge = await stripe.charges.create({
+      amount: 500,
+      currency: "usd",
+      source: req.body.id, // obtained with Stripe.js
+      description: "Charge for 5 credits"
+    });
   });
 };
