354f6098663e473e98c23a3af9f772ce8f7b5893
CJ Moro <scjmoro@gmail.com>
2019-03-21 20:36:32 -0700

    Handle email link route

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -7,7 +7,11 @@ const jobPostTemplate = require("../services/emailTemplates/jobPostTemplate");
 const Job = mongoose.model("jobs");
 
 module.exports = function jobRoutes(app) {
-  app.post("/api/job", requireLogin, requireCredits, (req, res) => {
+  app.get("/api/jobs/thanks", (req, res) => {
+    res.send("Thank you for clicking");
+  });
+
+  app.post("/api/job", requireLogin, requireCredits, async (req, res) => {
     const { name, title, description, skills, tags } = req.body;
 
     const job = new Job({
@@ -21,6 +25,15 @@ module.exports = function jobRoutes(app) {
     });
     const applicants = [{ email: "nassdropp@gmail.com", responded: false }];
     const mailer = new Mailer(job, applicants, jobPostTemplate(job));
-    mailer.send();
+    try {
+      await mailer.send();
+      await job.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+
+      res.send(user);
+    } catch (err) {
+      res.status(422).send(err);
+    }
   });
 };
