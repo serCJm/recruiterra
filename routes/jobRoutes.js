dd425fe22e9adb47ffcd300bd1a2cbcc202fc624
CJ Moro <scjmoro@gmail.com>
2019-03-21 18:58:58 -0700

    Change titile to subject

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
+const Mailer = require("../services/Mailer");
+const jobPostTemplate = require("../services/emailTemplates/jobPostTemplate");
 
 const Job = mongoose.model("jobs");
 
@@ -10,12 +12,15 @@ module.exports = function jobRoutes(app) {
 
     const job = new Job({
       name,
-      title,
+      subject: title,
       description,
       skills: skills.split(",").map(skill => skill.trim()),
       tags: tags.split(",").map(tag => tag.trim()),
       _user: req.user.id,
       lastUpdated: Date.now()
     });
+    const applicants = [{ email: "nassdropp@gmail.com", responded: false }];
+    const mailer = new Mailer(job, applicants, jobPostTemplate(job));
+    mailer.send();
   });
 };
