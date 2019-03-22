143a0fbdd0b00dd764f1af74ac932afe58d964aa
CJ Moro <scjmoro@gmail.com>
2019-03-21 18:59:33 -0700

    Add Mailer class

---
+++ services/Mailer.js
@@ -0,0 +1,28 @@
+const sgMail = require("@sendgrid/mail");
+const keys = require("../config/keys");
+
+class Mailer {
+  constructor({ subject }, applicants, content) {
+    this.message = {
+      to: this.formatAddresses(applicants),
+      from: "no-reply@recruiterra.com",
+      subject: subject,
+      html: content,
+      trackingSettings: {
+        clickTracking: { enable: true }
+      }
+    };
+    sgMail.setApiKey(keys.sendGridKey);
+  }
+
+  formatAddresses(applicants) {
+    return applicants.map(({ email }) => email);
+  }
+
+  async send() {
+    const response = await sgMail.sendMultiple(this.message);
+    return response;
+  }
+}
+
+module.exports = Mailer;
