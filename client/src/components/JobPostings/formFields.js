d0d641335fd91acf3ae059068cd4c0e39aa42101
CJ Moro <scjmoro@gmail.com>
2019-03-23 23:28:38 -0700

    Export formFields into Separate file

---
+++ client/src/components/JobPostings/formFields.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    name: "name",
+    label: "Job Name"
+  },
+  { name: "title", label: "Title" },
+  { name: "description", label: "Description" },
+  {
+    name: "skills",
+    label: "Skills"
+  },
+  {
+    name: "tags",
+    label: "Tags"
+  }
+];
