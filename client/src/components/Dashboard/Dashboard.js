6c7b02b93d64e6f0a25dd66adf8e40f742162e79
CJ Moro <scjmoro@gmail.com>
2019-04-06 15:20:40 -0700

    Add JobsList component

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,12 +1,16 @@
 import React from "react";
 import classes from "./Dashboard.module.css";
 import { Link } from "react-router-dom";
+import JobsList from "./JobsList/JobsList";
 
 const Dashboard = () => {
   return (
-    <Link to="/job-postings/new" className={classes.btn}>
-      Add
-    </Link>
+    <>
+      <JobsList />
+      <Link to="/job-postings/new" className={classes.btn}>
+        Add
+      </Link>
+    </>
   );
 };
 
