e542969d23bbd446e22d858ac01471f6c26a88d8
CJ Moro <scjmoro@gmail.com>
2019-04-12 00:00:52 -0700

    Create useIntersectObserver hook

---
+++ client/src/utils/hooks.js
@@ -0,0 +1,40 @@
+import { useState, useRef, useEffect } from "react";
+
+// taken from: https://medium.com/the-non-traditional-developer/how-to-use-an-intersectionobserver-in-a-react-hook-9fb061ac6cb5
+export function useIntersectObserver({
+  root = null,
+  rootMargin,
+  threshold = 0
+} = {}) {
+  if (
+    "IntersectionObserver" in window &&
+    "IntersectionObserverEntry" in window &&
+    "intersectionRatio" in window.IntersectionObserverEntry.prototype
+  ) {
+    const [entry, setEntry] = useState({});
+    const [node, setNode] = useState(null);
+
+    const observer = useRef(null);
+
+    function observerCb([entry]) {
+      return setEntry(entry);
+    }
+
+    useEffect(() => {
+      if (observer.current) observer.current.disconnect();
+      observer.current = new window.IntersectionObserver(observerCb, {
+        root,
+        rootMargin,
+        threshold
+      });
+
+      const { current: currentObserver } = observer;
+      if (node) currentObserver.observe(node);
+      return () => currentObserver.disconnect();
+    }, [node, root, rootMargin, threshold]);
+
+    return [setNode, entry];
+  } else {
+    console.log("IntersectionObserver not supported in this browser");
+  }
+}
