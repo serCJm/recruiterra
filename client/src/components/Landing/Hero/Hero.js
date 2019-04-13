4bd90df42e93ca79599c9ae6731f0758dc35988d
CJ Moro <scjmoro@gmail.com>
2019-04-12 21:46:27 -0700

    Add btn styling

--- client/src/components/Landing/Hero/Hero.js
+++ client/src/components/Landing/Hero/Hero.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 import classes from "./Hero.module.css";
 
 const Hero = () => {
-    return ( <section className={classes.hero}>
-        <h1>Post Jobs And Apply For Positions Easily With Recruiterra</h1>
-        <button>Get Started</button>
-      </section> );
-}
- 
+  return (
+    <section className={classes.hero}>
+      <h1>Post Jobs And Apply For Positions Easily With Recruiterra</h1>
+      <button className={classes.btn}>Get Started</button>
+    </section>
+  );
+};
+
 export default Hero;
