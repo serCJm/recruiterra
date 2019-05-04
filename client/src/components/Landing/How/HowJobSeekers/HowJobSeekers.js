13d9ebee77ca1c7f8ddedcb9e8db6bea98d3d888
CJ Moro <scjmoro@gmail.com>
2019-05-04 16:24:30 -0700

    Add content

--- client/src/components/Landing/How/HowJobSeekers/HowJobSeekers.js
+++ client/src/components/Landing/How/HowJobSeekers/HowJobSeekers.js
@@ -1,53 +1,56 @@
 import React from "react";
 import PropTypes from "prop-types";
-import workspaceSmall from "../../../../images/workspace-small.png";
-import workspaceBig from "../../../../images/workspace-big.png";
-import interviewSmall from "../../../../images/interview-small.png";
-import interviewBig from "../../../../images/interview-big.png";
-import hireSmall from "../../../../images/hire-small.png";
-import hireBig from "../../../../images/hire-big.png";
+import postAJobSmall from "../../../../images/post-a-job-small.png";
+import postAJobBig from "../../../../images/post-a-job-big.png";
+import applySmall from "../../../../images/apply-small.png";
+import applyBig from "../../../../images/apply-big.png";
+import emailSmall from "../../../../images/email-small.png";
+import emailBig from "../../../../images/email-big.png";
 import HowSteps from "../HowSteps/HowSteps";
 
 const contents = [
   {
-    title: "Post A Job",
+    title: "Add Your Resume",
     description:
-      "Submit your job description using our simple step-by-step form. Our system will automatically find right candidates from our database and contact them to apply. After submitting your job post, all you have to do is sip your coffee.",
+      "Submit your job resume using our simple step-by-step form. Our system will automatically notify you when relevant jobs become available.",
     image: (
       <img
-        src={workspaceSmall}
-        srcSet={`${workspaceSmall} 1x,
-        ${workspaceBig} 2x`}
+        src={postAJobSmall}
+        srcSet={`${postAJobSmall} 1x,
+        ${postAJobBig} 2x`}
         // sizes='(min-width: 960px) 960px,
         //        100vw'
+        alt="post-a-job"
       />
     )
   },
   {
-    title: "Screen Candidates",
+    title: "Apply For Jobs",
     description:
-      "Candidates who matched your criteria will be notified to apply for your position. For your matches, you will be able to access resume and references to easily screen for the right hire.",
+      "You will get notified about job postings relevant to your criteria. Right from your mailbox, you can apply with a single click to the jobs that fit what you are looking for.",
     image: (
       <img
-        src={interviewSmall}
-        srcSet={`${interviewSmall} 1x,
-        ${interviewBig} 2x`}
+        src={applySmall}
+        srcSet={`${applySmall} 1x,
+        ${applyBig} 2x`}
         // sizes='(min-width: 960px) 960px,
         //        100vw'
+        alt="apply"
       />
     )
   },
   {
-    title: "Hire",
+    title: "Interview And Get Hired",
     description:
-      "Once you are confident that a particular candiatate is the right fit for you, you mark a hire with one click. All other candidates will automatically be notifed with a rejection.",
+      "When employers act on your application, you will get an email notifying you about your status. If your application is successful, you will receive instructions on how to proceed with the interview.",
     image: (
       <img
-        src={hireSmall}
-        srcSet={`${hireSmall} 1x,
-        ${hireBig} 2x`}
+        src={emailSmall}
+        srcSet={`${emailSmall} 1x,
+        ${emailBig} 2x`}
         // sizes='(min-width: 960px) 960px,
         //        100vw'
+        alt="email"
       />
     )
   }
