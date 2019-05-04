7cf87126ea0b15595766801f71b4bcf5c9c003f7
CJ Moro <scjmoro@gmail.com>
2019-05-04 15:32:29 -0700

    Add tabs and animation logic

--- client/src/components/Landing/How/How.js
+++ client/src/components/Landing/How/How.js
@@ -1,14 +1,53 @@
-import React from 'react';
+import React, { useState } from "react";
 import classes from "./How.module.css";
-import HowEmployers from './HowEmployers/HowEmployers';
+import HowEmployers from "./HowEmployers/HowEmployers";
+import HowJobSeekers from "./HowJobSeekers/HowJobSeekers";
+
+const childAnimationTime = 500;
 
 const How = () => {
-    return ( <><section className={classes.howHero}>
-        <h2 className={classes.howHeroTitle}>How Recruiterra Makes Job Process Easy</h2>
-    </section>
-    <HowEmployers></HowEmployers>
-    
-    </> );
-}
- 
+  const [activeTab, setActiveTab] = useState(0);
+  const [activeContent, setActiveContent] = useState(0);
+  const [exitAnimation, setExitAnimation] = useState(false);
+
+  function handleTabSwitch(tabIndex) {
+    setActiveTab(tabIndex);
+    setExitAnimation(true);
+    setTimeout(() => {
+      setActiveContent(tabIndex);
+      setExitAnimation(false);
+    }, childAnimationTime);
+  }
+  return (
+    <>
+      <section className={classes.howHero}>
+        <h2 className={classes.howHeroTitle}>
+          How Recruiterra Makes Job Process Easy
+        </h2>
+      </section>
+      <section className={classes.container}>
+        <div className={classes.tabsContainer}>
+          <button
+            className={activeTab === 0 ? classes.activeTab : classes.tab}
+            onClick={() => handleTabSwitch(0)}
+          >
+            Employers
+          </button>
+          <button
+            className={activeTab === 1 ? classes.activeTab : classes.tab}
+            onClick={() => handleTabSwitch(1)}
+          >
+            Job Seekers
+          </button>
+        </div>
+        {activeContent === 0 ? (
+          <HowEmployers exitAnimation={exitAnimation} />
+        ) : (
+          <HowJobSeekers exitAnimation={exitAnimation} />
+        )}
+      </section>
+    </>
+  );
+};
+
 export default How;
