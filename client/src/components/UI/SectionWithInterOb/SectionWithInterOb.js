8f8fa36ec17e15fddd74bbd7c0c2658b1cffea9b
CJ Moro <scjmoro@gmail.com>
2019-05-15 00:53:19 -0700

    Dirty fix active link on scroll bug

--- client/src/components/UI/SectionWithInterOb/SectionWithInterOb.js
+++ client/src/components/UI/SectionWithInterOb/SectionWithInterOb.js
@@ -12,7 +12,24 @@ const SectionWithInterOb = ({ children, id, className }) => {
 
   useEffect(() => {
     if (observedEl.target) {
-      if (observedEl.intersectionRatio >= selected.ratio) {
+      // hard code active link when scrolling back from the bottom of the page
+      // due to html structure, "how" section is above and not visible at the point
+      // when starting to scroll back
+      // thus, without this dirty hack, it does not trigger an update in active links
+      // fix in future by changing html in "How" component - prob need to wrap whole section in this HOC and change its CSS to fit css grid
+      if (observedEl.target.id === "contact" && !observedEl.isIntersecting) {
+        selected.setActiveLink(prevState => {
+          return {
+            ...prevState,
+            id: "how",
+            ratio: 0
+          };
+        });
+      }
+      if (
+        observedEl.intersectionRatio >= selected.ratio ||
+        observedEl.isIntersecting
+      ) {
         selected.setActiveLink(prevState => {
           return {
             ...prevState,
@@ -22,7 +39,7 @@ const SectionWithInterOb = ({ children, id, className }) => {
         });
       }
     }
-  }, [observedEl.intersectionRatio]);
+  }, [observedEl.isIntersecting]);
 
   return (
     <section ref={setSectionRef} id={id} className={className}>
