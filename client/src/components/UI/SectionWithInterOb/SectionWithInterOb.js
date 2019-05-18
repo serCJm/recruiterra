1b81ecf3faac3ec80e3bcd97b20c19083adbceca
CJ Moro <scjmoro@gmail.com>
2019-05-18 01:22:36 -0700

    Update active link based on highest intersection

--- client/src/components/UI/SectionWithInterOb/SectionWithInterOb.js
+++ client/src/components/UI/SectionWithInterOb/SectionWithInterOb.js
@@ -1,45 +1,55 @@
 import React, { useEffect, useContext } from "react";
 import PropTypes from "prop-types";
 import { useIntersectObserver } from "../../../utils/hooks";
-import { ActiveNavLink } from "../../context";
+import { ActiveNavLink, LandingLinkRatios } from "../../context";
 
 const SectionWithInterOb = ({ children, id, className }) => {
   const [setSectionRef, observedEl] = useIntersectObserver({
-    threshold: new Array(11).fill(0).map((v, i) => i * 0.1)
+    threshold: new Array(101).fill(0).map((v, i) => i * 0.01)
   });
 
   const selected = useContext(ActiveNavLink);
+  const ratios = useContext(LandingLinkRatios);
 
   useEffect(() => {
-    if (observedEl.target) {
-      // hard code active link when scrolling back from the bottom of the page
-      // due to html structure, "how" section is above and not visible at the point
-      // when starting to scroll back
-      // thus, without this dirty hack, it does not trigger an update in active links
-      // fix in future by changing html in "How" component - prob need to wrap whole section in this HOC and change its CSS to fit css grid
-      if (observedEl.target.id === "contact" && !observedEl.isIntersecting) {
-        selected.setActiveLink(prevState => {
-          return {
-            ...prevState,
-            id: "how",
-            ratio: 0
+    if (observedEl.isIntersecting) {
+      ratios.setActiveRatio(prevState => {
+        return {
+          ...prevState,
+          [observedEl.target.id]: observedEl.intersectionRatio
+        };
+      });
+      // links = [["linkid", ratio], ...] -> [slice setFunction] -> [{id: "linkid", ratio: ratio}]
+      const links = Object.entries(ratios)
+        .slice(0, -1)
+        .reduce((acc, val) => {
+          const link = {
+            id: val[0],
+            ratio: val[1]
           };
-        });
-      }
-      if (
-        observedEl.intersectionRatio >= selected.ratio ||
-        observedEl.isIntersecting
-      ) {
-        selected.setActiveLink(prevState => {
-          return {
-            ...prevState,
-            id: observedEl.target.id,
-            ratio: observedEl.intersectionRatio
-          };
-        });
-      }
+          acc.push(link);
+          return acc;
+        }, []);
+      // activeLink = {id: "linkid", ratio: ratio} // with hightest ratio
+      const activeLink = links.reduce((acc, val) =>
+        val.ratio > acc.ratio ? val : acc
+      );
+      selected.setActiveLink(prevState => {
+        return {
+          ...prevState,
+          id: activeLink.id,
+          ratio: activeLink.ratio
+        };
+      });
+    } else if (observedEl.target && !observedEl.isIntersecting) {
+      ratios.setActiveRatio(prevState => {
+        return {
+          ...prevState,
+          [observedEl.target.id]: 0
+        };
+      });
     }
-  }, [observedEl.isIntersecting]);
+  }, [observedEl.intersectionRatio]);
 
   return (
     <section ref={setSectionRef} id={id} className={className}>
