17f0a844d5498d25a9eb09b1240974e917a3a0b4
CJ Moro <scjmoro@gmail.com>
2019-05-24 23:46:16 -0700

    Conditional render based on location

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,31 +1,41 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
 import classes from "./Footer.module.css";
 import SectionWithInterOb from "../UI/SectionWithInterOb/SectionWithInterOb";
 
-const Footer = () => {
+const Footer = ({ style, location }) => {
   return (
     <>
-      <SectionWithInterOb id="contact" className={classes.location}>
-        <address className={classes.address}>
-          Recruiterra
-          <br />
-          4444 Canada Way
-          <br />
-          H5N 1Z7
-          <br />
-          Vancouver, BC
-          <br />
-          Canada
-        </address>
-      </SectionWithInterOb>
-      <section className={classes.footer}>
-        <div className={classes.footerContainer}>
-          <p>Copyright © 2019 by Recruiterra.</p>
-          <p>All Right Reserved.</p>
-        </div>
-      </section>
+      {location === "/" ? (
+        <footer className={style}>
+          <SectionWithInterOb id="contact" className={classes.location}>
+            <address className={classes.address}>
+              Recruiterra
+              <br />
+              4444 Canada Way
+              <br />
+              H5N 1Z7
+              <br />
+              Vancouver, BC
+              <br />
+              Canada
+            </address>
+          </SectionWithInterOb>
+          <section className={classes.footer}>
+            <div className={classes.footerContainer}>
+              <p>Copyright © 2019 by Recruiterra.</p>
+              <p>All Right Reserved.</p>
+            </div>
+          </section>
+        </footer>
+      ) : null}
     </>
   );
 };
 
-export default Footer;
+Footer.propTypes = {
+  style: PropTypes.string
+};
+
+export default withRouter(Footer);
