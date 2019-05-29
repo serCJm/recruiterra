6cae52e1e320252f2091681eb6bd61d0029a468f
CJ Moro <scjmoro@gmail.com>
2019-05-29 00:40:54 -0700

    Add Spinner component

---
+++ client/src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,23 @@
+import React from "react";
+import classes from "./Spinner.module.css";
+
+const Spinner = () => {
+  return (
+    <div className={classes.ldsDefault}>
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  );
+};
+
+export default Spinner;
