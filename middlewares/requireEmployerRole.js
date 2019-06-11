426f4115fad10e2a34e9c1d746dcd2d27db076bf
cjmoro <scjmoro@gmail.com>
2019-06-11 01:05:22 -0700

    Fix check

--- middlewares/requireEmployerRole.js
+++ middlewares/requireEmployerRole.js
@@ -1,5 +1,5 @@
 module.exports = function requireEmployerRole(req, res, next) {
-  if (req.user.type !== "employer") {
+  if (req.user.usertype !== "employer") {
     return res.status(401).send({ error: "Invalid User Type For This Route" });
   }
   next();
