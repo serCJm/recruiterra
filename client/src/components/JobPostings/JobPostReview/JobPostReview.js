fdf9cbe11ff57271c1b81819d2ee891d3e182c98
cjmoro <scjmoro@gmail.com>
2019-06-13 22:33:23 -0700

    Add styling

--- client/src/components/JobPostings/JobPostReview/JobPostReview.js
+++ client/src/components/JobPostings/JobPostReview/JobPostReview.js
@@ -1,15 +1,17 @@
 import React from "react";
+import classes from "./JobPostReview.module.css";
 import { connect } from "react-redux";
 import formFields from "../formFields";
 import { withRouter } from "react-router-dom";
 import * as actions from "../../../store/actions";
+import RegBtn from "../../UI/Btns/RegBtn/RegBtn";
 
 const JobPostReview = ({ onCancel, formValues, submitJobPost, history }) => {
   function renderPostReview() {
     return formFields.map(({ label, name }) => (
       <div key={name}>
-        <label>{label}</label>
-        <div>{formValues[name]}</div>
+        <h4 className={classes.label}>{label}:</h4>
+        <p>{formValues[name]}</p>
       </div>
     ));
   }
@@ -21,11 +23,16 @@ const JobPostReview = ({ onCancel, formValues, submitJobPost, history }) => {
   }
 
   return (
-    <div>
-      <h5>Please review your job post</h5>
+    <div className={classes.container}>
       {renderPostReview()}
-      <button onClick={onCancel}>Back</button>
-      <button onClick={handleSubmitJobPost}>Post Job</button>
+      <div className={classes.btnContainer}>
+        <RegBtn btnStyle="danger" onClick={onCancel}>
+          Back
+        </RegBtn>
+        <RegBtn btnStyle="success" onClick={handleSubmitJobPost}>
+          Post Job
+        </RegBtn>
+      </div>
     </div>
   );
 };
