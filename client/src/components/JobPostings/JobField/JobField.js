76f3f1d71c8942f0024ae0f2dce39554d213fec4
cjmoro <scjmoro@gmail.com>
2019-06-13 22:34:48 -0700

    Add text area

--- client/src/components/JobPostings/JobField/JobField.js
+++ client/src/components/JobPostings/JobField/JobField.js
@@ -4,11 +4,19 @@ import classes from "./JobField.module.css";
 const JobField = ({ input, label, meta: { error, touched } }) => {
   return (
     <>
-      <p className={classes.inputContainer}>
-        <input {...input} className={classes.input} placeholder={label} />
+      <div className={classes.inputContainer}>
+        {input.name !== "description" ? (
+          <input {...input} className={classes.input} placeholder={label} />
+        ) : (
+          <textarea
+            {...input}
+            className={classes.textarea}
+            placeholder={label}
+          />
+        )}
         <label className={classes.label}>{label}</label>
         <div className={classes.error}>{touched && error}</div>
-      </p>
+      </div>
     </>
   );
 };
