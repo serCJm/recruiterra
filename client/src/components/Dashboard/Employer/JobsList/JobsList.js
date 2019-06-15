eb65849096456d9d8aa609a86725cb56462bd328
cjmoro <scjmoro@gmail.com>
2019-06-14 22:35:56 -0700

    Add spinner on loading

---
+++ client/src/components/Dashboard/Employer/JobsList/JobsList.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from "react";
+import classes from "./JobList.module.css";
+import { connect } from "react-redux";
+import { fetchJobs } from "../../../../store/actions";
+import Spinner from "../../../UI/Spinner/Spinner";
+
+const JobsList = props => {
+  console.log(props);
+  let jobList;
+  useEffect(() => {
+    props.fetchJobs();
+  }, []);
+  function renderJobList() {
+    if (props.loading) {
+      return <Spinner />;
+    } else if (props.jobs.length === 0) {
+      return (
+        <p>
+          You don't have any job posts. Please create one by clicking on the add
+          button.
+        </p>
+      );
+    }
+    return props.jobs.reverse().map(job => (
+      <div key={job._id}>
+        <h2>{job.name}</h2>
+        <p>{job.subject}</p>
+        <p>{job.description}</p>
+        <p>Posted on: {new Date(job.lastUpdated).toLocaleDateString()}</p>
+      </div>
+    ));
+  }
+  return <section className={classes.container}>{renderJobList()}</section>;
+};
+
+function mapStateToProps({ jobs }) {
+  return { jobs: jobs.jobsList, loading: jobs.loading };
+}
+
+export default connect(
+  mapStateToProps,
+  { fetchJobs }
+)(JobsList);
