7f1a91e9488697772e5a85c85b868f393c4477b6
cjmoro <scjmoro@gmail.com>
2019-06-18 13:00:23 -0700

    Add proptypes

--- client/src/components/UI/Btns/DotsBtn/DotsBtn.js
+++ client/src/components/UI/Btns/DotsBtn/DotsBtn.js
@@ -1,14 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 import classes from "./DotsBtn.module.css";
 
-const DotsBtn = () => {
+const DotsBtn = ({ onClick }) => {
   return (
-    <button className={classes.btnOptins}>
+    <button className={classes.btnOptins} onClick={onClick}>
       <span className={classes.circle} />
       <span className={classes.circle} />
       <span className={classes.circle} />
     </button>
   );
+};
+
+DotsBtn.propTypes = {
+  onClick: PropTypes.func
 };
 
 export default DotsBtn;
