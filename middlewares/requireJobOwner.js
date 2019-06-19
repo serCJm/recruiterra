a3357ae74c573347f6447cd2a96cbe31d094b469
cjmoro <scjmoro@gmail.com>
2019-06-18 23:12:22 -0700

    Create new middleware

---
+++ middlewares/requireJobOwner.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Job = mongoose.model("jobs");
+module.exports = async function requireJobOwner(req, res, next) {
+  try {
+    let job = await Job.findById(req.body.id);
+    if (req.user.id !== job._user.toString()) {
+      return res.status(401).send({ error: "You do not own this job." });
+    }
+    next();
+  } catch (e) {
+    return res.status(500).send({ error: "Something went wrong." });
+  }
+};
