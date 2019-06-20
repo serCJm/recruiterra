cd6a982f94db20b930dae83299ee1e99d7402d70
cjmoro <scjmoro@gmail.com>
2019-06-19 23:55:42 -0700

    Add OptionsMenu component

--- client/src/components/UI/OptionsMenu/OptionsMenu.js
+++ client/src/components/UI/OptionsMenu/OptionsMenu.js
@@ -1,8 +1,32 @@
 import React from "react";
+import { connect } from "react-redux";
 import RegBtn from "../Btns/RegBtn/RegBtn";
 import classes from "./OptionsMenu.module.css";
+import { deleteJobs, updateCurrentJobId } from "../../../store/actions";
+import { withRouter } from "react-router-dom";
 
-const OptionsMenu = ({ status }) => {
+const OptionsMenu = ({
+  status,
+  job,
+  deleteJobs,
+  history,
+  updateCurrentJobId
+}) => {
+  function handleDelteJob(jobId) {
+    if (
+      window.confirm(
+        "Are you sure you want to delete this? You won't be able to undo post deletion."
+      )
+    ) {
+      return deleteJobs(jobId);
+    }
+    return;
+  }
+
+  function handleUpdateJob(jobId) {
+    updateCurrentJobId(jobId);
+    return history.push("/job-postings/update");
+  }
   return (
     <>
       {status !== "closed" && (
@@ -11,12 +35,21 @@ const OptionsMenu = ({ status }) => {
             status === "closing" ? classes.containerClose : classes.container
           }
         >
-          <RegBtn btnStyle="danger">Delete</RegBtn>
-          <RegBtn btnStyle="success">Edit</RegBtn>
+          <RegBtn btnStyle="danger" onClick={() => handleDelteJob(job._id)}>
+            Delete
+          </RegBtn>
+          <RegBtn btnStyle="success" onClick={() => handleUpdateJob(job._id)}>
+            Edit
+          </RegBtn>
         </div>
       )}
     </>
   );
 };
 
-export default OptionsMenu;
+export default withRouter(
+  connect(
+    null,
+    { deleteJobs, updateCurrentJobId }
+  )(OptionsMenu)
+);
