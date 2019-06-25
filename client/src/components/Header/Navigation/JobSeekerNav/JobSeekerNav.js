1674a1c9a224d980f5bdf9b641ee10e13af59fde
cjmoro <scjmoro@gmail.com>
2019-06-25 15:09:29 -0700

    Add JobSeekerNav component

---
+++ client/src/components/Header/Navigation/JobSeekerNav/JobSeekerNav.js
@@ -0,0 +1,22 @@
+import React from "react";
+import classes from "./JobSeekerNav.module.css";
+import { Link } from "react-router-dom";
+
+const JobSeekerNav = ({ handleUserLogout }) => {
+  return (
+    <>
+      <li className={classes.creditsContainer}>
+        <Link to="job-invitations" className={classes.link}>
+          Invitations
+        </Link>
+      </li>
+      <li className={classes.navItemContainer}>
+        <button className={classes.linkBtn} onClick={handleUserLogout}>
+          Logout
+        </button>
+      </li>
+    </>
+  );
+};
+
+export default JobSeekerNav;
