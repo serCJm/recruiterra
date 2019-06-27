a06b347c993126cf1ee3b2852cb35a452d1eccf4
cjmoro <scjmoro@gmail.com>
2019-06-27 00:07:10 -0700

    Add create route

--- routes/jobSeekerRoutes.js
+++ routes/jobSeekerRoutes.js
@@ -1,1 +1,9 @@
-module.exports = function jobSeekerRoutes(app) {};
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
+const requireJobSeekerRole = require("../middlewares/requireJobSeekerRole")
+
+module.exports = function jobSeekerRoutes(app) {
+  app.post("/api/resumes/create", requireLogin, requireJobSeekerRole, (req, res) => {
+    res.send(req.user);
+  });
+};
