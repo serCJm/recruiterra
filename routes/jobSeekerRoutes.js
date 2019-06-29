886a49720425587552e1b65966666e92048b2cc7
cjmoro <scjmoro@gmail.com>
2019-06-29 01:31:37 -0700

    Handle create route

--- routes/jobSeekerRoutes.js
+++ routes/jobSeekerRoutes.js
@@ -1,9 +1,50 @@
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
-const requireJobSeekerRole = require("../middlewares/requireJobSeekerRole")
+const requireJobSeekerRole = require("../middlewares/requireJobSeekerRole");
+
+const Resume = mongoose.model("resumes");
 
 module.exports = function jobSeekerRoutes(app) {
-  app.post("/api/resumes/create", requireLogin, requireJobSeekerRole, (req, res) => {
-    res.send(req.user);
-  });
+  app.post(
+    "/api/resumes/create",
+    requireLogin,
+    requireJobSeekerRole,
+    async (req, res) => {
+      const {
+        resumeName,
+        fullName,
+        summary,
+        education,
+        skills,
+        experience,
+        tags
+      } = req.body;
+
+      const resume = new Resume({
+        resumeName,
+        fullName,
+        summary,
+        education: splitAndTrim(education),
+        skills: splitAndTrim(skills),
+        experience: splitAndTrim(experience),
+        tags: splitAndTrim(tags),
+        _user: req.user.id,
+        lastUpdated: Date.now()
+      });
+      try {
+        await resume.save();
+        res.send(req.user);
+      } catch (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
 };
+
+// HELPERS
+function splitAndTrim(arrOfStrgs) {
+  return arrOfStrgs
+    .split(/[,;]+/)
+    .map(string => string.trim())
+    .filter(Boolean);
+}
