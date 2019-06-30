39234115a10792c248f4fe673819a44ec1e7233a
cjmoro <scjmoro@gmail.com>
2019-06-30 00:14:33 -0700

    Add ResumesList

--- client/src/components/Dashboard/JobSeeker/Dashboard.js
+++ client/src/components/Dashboard/JobSeeker/Dashboard.js
@@ -1,11 +1,13 @@
 import React from "react";
 import classes from "./Dashboard.module.css";
 import { Link } from "react-router-dom";
+import ResumeList from "./ResumeList/ResumeList";
 // import JobsList from "./JobsList/JobsList";
 
 const Dashboard = () => {
   return (
     <>
+      <ResumeList />
       <Link to="/resumes/new" className={classes.btn}>
         Add
       </Link>
