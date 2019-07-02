26429edf08c88ee0d203a0a612aa80573e36fdc8
cjmoro <scjmoro@gmail.com>
2019-07-01 23:18:16 -0700

    Add component to update a resume item

---
+++ client/src/components/Resumes/ResumeUpdate/ResumeUpdate.js
@@ -0,0 +1,85 @@
+import React from "react";
+import classes from "../ResumeForm/ResumeForm.module.css";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { reduxForm, Field } from "redux-form";
+import ResumeField from "../ResumeField/ResumeField";
+import { Link } from "react-router-dom";
+//import validateEmails from "../../../utils/validateEmails";
+import formFields from "../formFields";
+import RegBtn from "../../UI/Btns/RegBtn/RegBtn";
+import { updateResumes } from "../../../store/actions";
+
+// NOTE: there's a bug because if page gets reloaded on this component
+// state will be lost causing error in mapStateToProps
+// TODO: save edited job into local storage - DONE
+const ResumeUpdate = props => {
+  async function handleResumeUpdate(formValues) {
+    await props.updateResumes(formValues, props.currentResumeId);
+    props.history.push("/my-resumes");
+  }
+  function renderFields() {
+    return formFields.map(({ name, label }) => {
+      return (
+        <Field
+          key={name}
+          component={ResumeField}
+          type="text"
+          name={name}
+          label={label}
+        />
+      );
+    });
+  }
+  return (
+    <section className={classes.formContainer}>
+      <form onSubmit={props.handleSubmit(handleResumeUpdate)}>
+        {renderFields()}
+        <div className={classes.btnContainer}>
+          <Link to="/my-resumes">
+            <RegBtn btnStyle="danger">Cancel</RegBtn>
+          </Link>
+          <RegBtn btnStyle="success" type="submit">
+            Update
+          </RegBtn>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+function validate(values) {
+  const errors = {};
+
+  formFields.forEach(({ name }) => {
+    if (!values[name]) {
+      errors[name] = `You must provide a ${name.replace(/s+$/, "")}`;
+    }
+  });
+
+  return errors;
+}
+
+const mapStateToProps = ({ resumes }) => {
+  const values = localStorage.getItem("resumeValues");
+  return {
+    initialValues: JSON.parse(values),
+    currentResumeId: localStorage.getItem("currentResumeId")
+  };
+};
+
+ResumeUpdate.propTypes = {
+  currentJobId: PropTypes.string,
+  updateResumes: PropTypes.func
+};
+
+export default connect(
+  mapStateToProps,
+  { updateResumes }
+)(
+  reduxForm({
+    validate,
+    form: "resumeFormUpdate",
+    destroyOnUnmount: true
+  })(ResumeUpdate)
+);
