b38666177a86caff7091aa34cb0c8b232292fe0f
cjmoro <scjmoro@gmail.com>
2019-07-01 22:16:12 -0700

    Export JobsItem options into separate logic

---
+++ client/src/components/Dashboard/Employer/JobItemOptions/JobItemOptions.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { connect } from "react-redux";
+import { deleteJobs } from "../../../../store/actions";
+import { withRouter } from "react-router-dom";
+import OptionsMenu from "../../../UI/OptionsMenu/OptionsMenu";
+
+const JobItemOptions = ({ status, job, deleteJobs, history }) => {
+  function handleDelteJob() {
+    if (
+      window.confirm(
+        "Are you sure you want to delete this? You won't be able to undo post deletion."
+      )
+    ) {
+      return deleteJobs(job._id);
+    }
+    return;
+  }
+
+  function handleUpdateJob() {
+    console.log(job);
+    localStorage.setItem(
+      "jobValues",
+      JSON.stringify({
+        name: job.name,
+        title: job.title,
+        description: job.description,
+        skills: job.skills.join(", "),
+        tags: job.tags.join(", ")
+      })
+    );
+    localStorage.setItem("currentJobId", job._id);
+    return history.push("/job-postings/update");
+  }
+  return (
+    <>
+      {status !== "closed" && (
+        <OptionsMenu
+          status={status}
+          onClickDelete={handleDelteJob}
+          onClickUpdate={handleUpdateJob}
+        />
+      )}
+    </>
+  );
+};
+
+export default withRouter(
+  connect(
+    null,
+    { deleteJobs }
+  )(JobItemOptions)
+);
--- client/src/components/UI/OptionsMenu/OptionsMenu.js
+++ client/src/components/UI/OptionsMenu/OptionsMenu.js
@@ -1,60 +1,22 @@
 import React from "react";
-import { connect } from "react-redux";
 import RegBtn from "../Btns/RegBtn/RegBtn";
 import classes from "./OptionsMenu.module.css";
-import { deleteJobs } from "../../../store/actions";
-import { withRouter } from "react-router-dom";
 
-const OptionsMenu = ({ status, job, deleteJobs, history }) => {
-  function handleDelteJob(jobId) {
-    if (
-      window.confirm(
-        "Are you sure you want to delete this? You won't be able to undo post deletion."
-      )
-    ) {
-      return deleteJobs(jobId);
-    }
-    return;
-  }
-
-  function handleUpdateJob(jobId) {
-    console.log(job);
-    localStorage.setItem(
-      "jobValues",
-      JSON.stringify({
-        name: job.name,
-        title: job.title,
-        description: job.description,
-        skills: job.skills.join(", "),
-        tags: job.tags.join(", ")
-      })
-    );
-    localStorage.setItem("currentJobId", jobId);
-    return history.push("/job-postings/update");
-  }
+const OptionsMenu = ({ status, onClickDelete, onClickUpdate }) => {
   return (
-    <>
-      {status !== "closed" && (
-        <div
-          className={
-            status === "closing" ? classes.containerClose : classes.container
-          }
-        >
-          <RegBtn btnStyle="danger" onClick={() => handleDelteJob(job._id)}>
-            Delete
-          </RegBtn>
-          <RegBtn btnStyle="success" onClick={() => handleUpdateJob(job._id)}>
-            Edit
-          </RegBtn>
-        </div>
-      )}
-    </>
+    <div
+      className={
+        status === "closing" ? classes.containerClose : classes.container
+      }
+    >
+      <RegBtn btnStyle="danger" onClick={() => onClickDelete()}>
+        Delete
+      </RegBtn>
+      <RegBtn btnStyle="success" onClick={() => onClickUpdate()}>
+        Edit
+      </RegBtn>
+    </div>
   );
 };
 
-export default withRouter(
-  connect(
-    null,
-    { deleteJobs }
-  )(OptionsMenu)
-);
+export default OptionsMenu;
