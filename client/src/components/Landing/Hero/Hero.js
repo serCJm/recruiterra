7ee754e4553da0fc8fa2e75e44829263e201bd3c
cjmoro <scjmoro@gmail.com>
2019-07-21 00:44:42 -0700

    Set background based on webp support

--- client/src/components/Landing/Hero/Hero.js
+++ client/src/components/Landing/Hero/Hero.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import classes from "./Hero.module.css";
 
 const Hero = () => {
+  const [heroClass, setHeroClass] = useState(classes.heroNoWebP);
+  /* global Modernizr */
+
+  console.log(Modernizr);
+
+  useEffect(() => {
+    if (Modernizr.webp) {
+      console.log(Modernizr);
+      setHeroClass(classes.heroWebP);
+    }
+  }, []);
   return (
-    <section className={classes.hero}>
+    <section className={heroClass}>
       <h1>Post Jobs And Apply For Positions Easily With Recruiterra</h1>
       <button className={classes.btn}>Get Started</button>
     </section>
