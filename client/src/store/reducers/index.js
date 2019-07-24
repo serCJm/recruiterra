c7be8bc76940615be4e4d161abba454b25f4a571
cjmoro <scjmoro@gmail.com>
2019-07-24 00:48:35 -0700

    Add activeLink reducer

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -3,10 +3,12 @@ import { reducer as reduxForm } from "redux-form";
 import authReducer from "./authReducer";
 import jobsReducer from "./jobsReducer";
 import resumesReducer from "./resumesReducer";
+import activeLinkReducer from "./activeLinkReducer";
 
 export default combineReducers({
   auth: authReducer,
   jobs: jobsReducer,
   resumes: resumesReducer,
-  form: reduxForm
+  form: reduxForm,
+  activeLink: activeLinkReducer
 });
