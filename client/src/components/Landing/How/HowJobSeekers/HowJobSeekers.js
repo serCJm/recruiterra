a13712f43cecd3609820ad7a01983a7743cd4135
cjmoro <scjmoro@gmail.com>
2019-08-07 00:24:06 -0700

    Add image optimizations

--- client/src/components/Landing/How/HowJobSeekers/HowJobSeekers.js
+++ client/src/components/Landing/How/HowJobSeekers/HowJobSeekers.js
@@ -1,14 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 import postAJobSmall from "../../../../images/post-a-job-small.png";
+import postAJobSmallWebp from "../../../../images/post-a-job-small.webp";
+import postAJobSmallJP2 from "../../../../images/post-a-job-small.jp2";
 import postAJobBig from "../../../../images/post-a-job-big.png";
 import postAJobBigWebp from "../../../../images/post-a-job-big.webp";
 import postAJobBigJP2 from "../../../../images/post-a-job-big.jp2";
 import applySmall from "../../../../images/apply-small.png";
+import applySmallWebp from "../../../../images/apply-small.webp";
+import applySmallJP2 from "../../../../images/apply-small.jp2";
 import applyBig from "../../../../images/apply-big.png";
 import applyBigWebp from "../../../../images/apply-big.webp";
 import applyBigJP2 from "../../../../images/apply-big.jp2";
 import emailSmall from "../../../../images/email-small.png";
+import emailSmallWebp from "../../../../images/email-small.webp";
+import emailSmallJP2 from "../../../../images/email-small.jp2";
 import emailBig from "../../../../images/email-big.png";
 import emailBigWebp from "../../../../images/email-big.webp";
 import emailBigJP2 from "../../../../images/email-big.jp2";
@@ -21,8 +27,14 @@ const contents = [
       "Submit your job resume using our simple step-by-step form. Our system will automatically notify you when relevant jobs become available.",
     image: (
       <picture>
-        <source type="image/webp" srcSet={postAJobBigWebp} />
-        <source type="image/jp2" srcSet={postAJobBigJP2} />
+        <source
+          type="image/webp"
+          srcSet={`${postAJobSmallWebp} 1x, ${postAJobBigWebp} 2x`}
+        />
+        <source
+          type="image/jp2"
+          srcSet={`${postAJobSmallJP2} 1x, ${postAJobBigJP2} 2x`}
+        />
         <img
           src={postAJobSmall}
           srcSet={`${postAJobSmall} 1x,
@@ -40,8 +52,14 @@ const contents = [
       "You will get notified about job postings relevant to your criteria. Right from your mailbox, you can apply with a single click to the jobs that fit what you are looking for.",
     image: (
       <picture>
-        <source type="image/webp" srcSet={applyBigWebp} />
-        <source type="image/jp2" srcSet={applyBigJP2} />
+        <source
+          type="image/webp"
+          srcSet={`${applySmallWebp} 1x, ${applyBigWebp} 2x`}
+        />
+        <source
+          type="image/jp2"
+          srcSet={`${applySmallJP2} 1x, ${applyBigJP2} 2x`}
+        />
         <img
           src={applySmall}
           srcSet={`${applySmall} 1x,
@@ -59,8 +77,14 @@ const contents = [
       "When employers act on your application, you will get an email notifying you about your status. If your application is successful, you will receive instructions on how to proceed with the interview.",
     image: (
       <picture>
-        <source type="image/webp" srcSet={emailBigWebp} />
-        <source type="image/jp2" srcSet={emailBigJP2} />
+        <source
+          type="image/webp"
+          srcSet={`${emailSmallWebp} 1x, ${emailBigWebp} 2x`}
+        />
+        <source
+          type="image/jp2"
+          srcSet={`${emailSmallJP2} 1x, ${emailBigJP2} 2x`}
+        />
         <img
           src={emailSmall}
           srcSet={`${emailSmall} 1x,
