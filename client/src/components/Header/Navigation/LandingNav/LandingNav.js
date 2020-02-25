eae1efa5e0f1322a715604bb6253d0891e2be19b
cjmoro <scjmoro@gmail.com>
2020-02-25 10:58:58 -0800

    Change formatting

--- client/src/components/Header/Navigation/LandingNav/LandingNav.js
+++ client/src/components/Header/Navigation/LandingNav/LandingNav.js
@@ -6,77 +6,77 @@ import { ActiveNavLink } from "../../../context/ActiveLandingLinkProvider";
 import { smoothScroll } from "../../../../utils/helpers";
 
 const contents = [
-  {
-    href: "#about",
-    altHref: "/#about",
-    text: "About",
-    onClick: el => smoothScroll(el)
-  },
-  {
-    href: "#how",
-    altHref: "/#how",
-    text: "How",
-    onClick: el => smoothScroll(el)
-  },
-  {
-    href: "#contact",
-    altHref: "/#contact",
-    text: "Contact",
-    onClick: el => smoothScroll(el)
-  },
-  { href: "/sign-up", text: "Sign Up" },
-  { href: "/log-in", text: "Log In" }
+	{
+		href: "#about",
+		altHref: "/#about",
+		text: "About",
+		onClick: el => smoothScroll(el)
+	},
+	{
+		href: "#how",
+		altHref: "/#how",
+		text: "How",
+		onClick: el => smoothScroll(el)
+	},
+	{
+		href: "#contact",
+		altHref: "/#contact",
+		text: "Contact",
+		onClick: el => smoothScroll(el)
+	},
+	{ href: "/sign-up", text: "Sign Up" },
+	{ href: "/log-in", text: "Log In" }
 ];
 
 const LandingNav = ({ containerClass, linkClass, isLanding, transparent }) => {
-  const selected = useContext(ActiveNavLink);
+	const selected = useContext(ActiveNavLink);
 
-  function renderContent(item) {
-    if (item.text === "Sign Up" || item.text === "Log In") {
-      return (
-        <NavLink
-          to={item.href}
-          className={linkClass}
-          activeClassName={classes.active}
-        >
-          {item.text}
-        </NavLink>
-      );
-    } else {
-      return (
-        <a
-          href={isLanding ? item.href : item.altHref}
-          className={
-            !transparent &&
-            isLanding &&
-            selected.id === item.text.toLocaleLowerCase()
-              ? `${linkClass} ${classes.active}`
-              : linkClass
-          }
-          onClick={item.onClick}
-        >
-          {item.text}
-        </a>
-      );
-    }
-  }
+	function renderContent(item) {
+		if (item.text === "Sign Up" || item.text === "Log In") {
+			return (
+				<NavLink
+					to={item.href}
+					className={linkClass}
+					activeClassName={classes.active}
+				>
+					{item.text}
+				</NavLink>
+			);
+		} else {
+			return (
+				<a
+					href={isLanding ? item.href : item.altHref}
+					className={
+						!transparent &&
+						isLanding &&
+						selected.id === item.text.toLocaleLowerCase()
+							? `${linkClass} ${classes.active}`
+							: linkClass
+					}
+					onClick={item.onClick}
+				>
+					{item.text}
+				</a>
+			);
+		}
+	}
 
-  return (
-    <>
-      {contents.map(item => (
-        <li key={item.text} className={containerClass}>
-          {renderContent(item)}
-        </li>
-      ))}
-    </>
-  );
+	return (
+		<>
+			{contents.map(item => (
+				<li key={item.text} className={containerClass}>
+					{renderContent(item)}
+				</li>
+			))}
+		</>
+	);
 };
 
 LandingNav.propTypes = {
-  containerClass: PropTypes.string,
-  linkClass: PropTypes.string,
-  isLanding: PropTypes.bool,
-  transparent: PropTypes.bool
+	containerClass: PropTypes.string,
+	linkClass: PropTypes.string,
+	isLanding: PropTypes.bool,
+	transparent: PropTypes.bool
 };
 
 export default LandingNav;
