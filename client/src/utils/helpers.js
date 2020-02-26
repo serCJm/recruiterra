ba6f3d726eb1585cc3186692247fa849118c6cb3
cjmoro <scjmoro@gmail.com>
2020-02-26 00:22:57 -0800

    Add findByTestAttr

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,29 +1,34 @@
 export function smoothScroll(element, duration = 1000) {
-  const target = element.target.getAttribute("href");
-  const targetPosition = document.querySelector(target).offsetTop;
-  const startPosition = window.pageYOffset;
-  const navBarOffset = document.querySelector("nav").offsetHeight;
-  const distance = targetPosition - navBarOffset - startPosition;
+	const target = element.target.getAttribute("href");
+	const targetPosition = document.querySelector(target).offsetTop;
+	const startPosition = window.pageYOffset;
+	const navBarOffset = document.querySelector("nav").offsetHeight;
+	const distance = targetPosition - navBarOffset - startPosition;
 
-  let start = null;
+	let start = null;
 
-  window.requestAnimationFrame(step);
+	window.requestAnimationFrame(step);
 
-  function step(timestamp) {
-    if (!start) start = timestamp;
-    const progress = timestamp - start;
-    // window.scrollTo(0, distance*(progress/duration) + startPosition);
-    window.scrollTo(
-      0,
-      easeInOutCubic(progress, startPosition, distance, duration)
-    );
-    if (progress < duration) window.requestAnimationFrame(step);
-  }
+	function step(timestamp) {
+		if (!start) start = timestamp;
+		const progress = timestamp - start;
+		// window.scrollTo(0, distance*(progress/duration) + startPosition);
+		window.scrollTo(
+			0,
+			easeInOutCubic(progress, startPosition, distance, duration)
+		);
+		if (progress < duration) window.requestAnimationFrame(step);
+	}
 
-  function easeInOutCubic(t, b, c, d) {
-    t /= d / 2;
-    if (t < 1) return (c / 2) * t * t * t + b;
-    t -= 2;
-    return (c / 2) * (t * t * t + 2) + b;
-  }
+	function easeInOutCubic(t, b, c, d) {
+		t /= d / 2;
+		if (t < 1) return (c / 2) * t * t * t + b;
+		t -= 2;
+		return (c / 2) * (t * t * t + 2) + b;
+	}
+}
+
+export function findByTestAttr(component, attr) {
+	const wrapper = component.find(`[data-test="${attr}"]`);
+	return wrapper;
 }
