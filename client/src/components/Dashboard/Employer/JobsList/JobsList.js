ed290d202fa8ef32ce8f6c8d010c742c29e7889d
cjmoro <scjmoro@gmail.com>
2020-02-27 00:40:49 -0800

    Refactor to handle async

--- client/src/components/Dashboard/Employer/JobsList/JobsList.js
+++ client/src/components/Dashboard/Employer/JobsList/JobsList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback } from "react";
+import React, { useEffect } from "react";
 import classes from "./JobList.module.css";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
@@ -7,38 +7,39 @@ import Spinner from "../../../UI/Spinner/Spinner";
 import JobItem from "../JobItem/JobItem";
 
 const JobsList = props => {
-  const { fetchJobs } = props;
-  const fetchJobsHandler = useCallback(() => fetchJobs(), [fetchJobs]);
+	const { fetchJobs } = props;
 
-  useEffect(() => fetchJobsHandler(), [fetchJobsHandler]);
+	useEffect(() => {
+		async function fetchJobsHandler() {
+			await fetchJobs();
+		}
+		fetchJobsHandler();
+	}, [fetchJobs]);
 
-  function renderJobList() {
-    if (props.loading) {
-      return <Spinner />;
-    } else if (props.jobs.length === 0) {
-      return (
-        <p className={classes.emptyText}>
-          You don't have any job posts. Please create one by clicking on the add
-          button.
-        </p>
-      );
-    }
-    return props.jobs.reverse().map(job => <JobItem key={job._id} job={job} />);
-  }
-  return <section className={classes.container}>{renderJobList()}</section>;
+	function renderJobList() {
+		if (props.loading) {
+			return <Spinner />;
+		} else if (props.jobs.length === 0) {
+			return (
+				<p className={classes.emptyText}>
+					You don't have any job posts. Please create one by clicking on the add
+					button.
+				</p>
+			);
+		}
+		return props.jobs.reverse().map(job => <JobItem key={job._id} job={job} />);
+	}
+	return <section className={classes.container}>{renderJobList()}</section>;
 };
 
 function mapStateToProps({ jobs }) {
-  return { jobs: jobs.jobsList, loading: jobs.loading };
+	return { jobs: jobs.jobsList, loading: jobs.loading };
 }
 
 JobsList.propTypes = {
-  fetchJobs: PropTypes.func,
-  loading: PropTypes.bool,
-  jobs: PropTypes.array
+	fetchJobs: PropTypes.func,
+	loading: PropTypes.bool,
+	jobs: PropTypes.array
 };
 
-export default connect(
-  mapStateToProps,
-  { fetchJobs }
-)(JobsList);
+export default connect(mapStateToProps, { fetchJobs })(JobsList);
