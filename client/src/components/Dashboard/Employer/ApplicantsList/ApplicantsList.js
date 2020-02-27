81395a2ebc6d4eae800c71aa17e17033f1907b98
cjmoro <scjmoro@gmail.com>
2020-02-27 00:50:03 -0800

    Refactor to use async func

--- client/src/components/Dashboard/Employer/ApplicantsList/ApplicantsList.js
+++ client/src/components/Dashboard/Employer/ApplicantsList/ApplicantsList.js
@@ -7,44 +7,43 @@ import Spinner from "../../../UI/Spinner/Spinner";
 import ApplicantsItem from "./ApplicantsItem/ApplicantsItem";
 
 const ApplicantsList = props => {
-  const { fetchApplicants } = props;
-  const fetchApplicantsHandler = useCallback(() => fetchApplicants(), [
-    fetchApplicants
-  ]);
+	const { fetchApplicants } = props;
 
-  useEffect(() => fetchApplicantsHandler(), [fetchApplicantsHandler]);
+	useEffect(() => {
+		async function fetchApplicantsHandler() {
+			await fetchApplicants();
+		}
+		fetchApplicantsHandler();
+	}, [fetchApplicants]);
 
-  function renderApplicantsList() {
-    if (props.loading) {
-      return <Spinner />;
-    } else if (props.applicants.length === 0) {
-      return (
-        <p className={classes.emptyText}>
-          You don't have any applicants. Please create a job post and wait for
-          job seekers to apply.
-        </p>
-      );
-    }
-    return props.applicants
-      .reverse()
-      .map(resume => <ApplicantsItem key={resume._id} resume={resume} />);
-  }
-  return (
-    <section className={classes.container}>{renderApplicantsList()}</section>
-  );
+	function renderApplicantsList() {
+		if (props.loading) {
+			return <Spinner />;
+		} else if (props.applicants.length === 0) {
+			return (
+				<p className={classes.emptyText}>
+					You don't have any applicants. Please create a job post and wait for
+					job seekers to apply.
+				</p>
+			);
+		}
+		return props.applicants
+			.reverse()
+			.map(resume => <ApplicantsItem key={resume._id} resume={resume} />);
+	}
+	return (
+		<section className={classes.container}>{renderApplicantsList()}</section>
+	);
 };
 
 function mapStateToProps({ jobs }) {
-  return { applicants: jobs.applicantsList, loading: jobs.loading };
+	return { applicants: jobs.applicantsList, loading: jobs.loading };
 }
 
 ApplicantsList.propTypes = {
-  fetchApplicants: PropTypes.func,
-  loading: PropTypes.bool,
-  applicantsList: PropTypes.array
+	fetchApplicants: PropTypes.func,
+	loading: PropTypes.bool,
+	applicantsList: PropTypes.array
 };
 
-export default connect(
-  mapStateToProps,
-  { fetchApplicants }
-)(ApplicantsList);
+export default connect(mapStateToProps, { fetchApplicants })(ApplicantsList);
