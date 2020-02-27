975be32207b59024e8e0e62b5bbca7ad39f6163d
cjmoro <scjmoro@gmail.com>
2020-02-27 00:41:05 -0800

    Comment out dev dep

--- client/src/index.js
+++ client/src/index.js
@@ -7,19 +7,19 @@ import reduxThunk from "redux-thunk";
 import App from "./components/App";
 import reducers from "./store/reducers";
 // dev only axios helper
-import axios from "axios";
+// import axios from "axios";
 
 import "./index.css";
 import typography from "./typography";
 typography.injectStyles();
 // dev only axios helper
-window.axios = axios;
+// window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.querySelector("#root")
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.querySelector("#root")
 );
