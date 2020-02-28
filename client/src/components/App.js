b902413ea24953c160ee3c2a75601c7500f0905b
cjmoro <scjmoro@gmail.com>
2020-02-28 00:04:24 -0800

    Refactor fetchUser

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback, Suspense } from "react";
+import React, { useEffect, Suspense } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { connect } from "react-redux";
 import { fetchUser } from "../store/actions";
@@ -17,104 +17,102 @@ const LogIn = React.lazy(() => import("./LogIn/LogIn"));
 
 const Dashboard = React.lazy(() => import("./Dashboard/Employer/Dashboard"));
 const JobPostNew = React.lazy(() =>
-  import("./JobPostings/JobPostNew/JobPostNew")
+	import("./JobPostings/JobPostNew/JobPostNew")
 );
 const JobPostUpdate = React.lazy(() =>
-  import("./JobPostings/JobPostUpdate/JobPostUpdate")
+	import("./JobPostings/JobPostUpdate/JobPostUpdate")
 );
 const JobSeekerDashboard = React.lazy(() =>
-  import("./Dashboard/JobSeeker/Dashboard")
+	import("./Dashboard/JobSeeker/Dashboard")
 );
 const ResumeNew = React.lazy(() => import("./Resumes/ResumeNew/ResumeNew"));
 const ResumeUpdate = React.lazy(() =>
-  import("./Resumes/ResumeUpdate/ResumeUpdate")
+	import("./Resumes/ResumeUpdate/ResumeUpdate")
 );
 
 const EmailClickResponse = React.lazy(() =>
-  import("./EmailClickResponse/EmailClickResponse")
+	import("./EmailClickResponse/EmailClickResponse")
 );
 const ApplicantsList = React.lazy(() =>
-  import("./Dashboard/Employer/ApplicantsList/ApplicantsList")
+	import("./Dashboard/Employer/ApplicantsList/ApplicantsList")
 );
 const ApplicantsReview = React.lazy(() =>
-  import(
-    "./Dashboard/Employer/ApplicantsList/ApplicantsReview/ApplicantsReview"
-  )
+	import(
+		"./Dashboard/Employer/ApplicantsList/ApplicantsReview/ApplicantsReview"
+	)
 );
 
 const App = ({ auth, fetchUser }) => {
-  const fetchUserHandler = useCallback(() => fetchUser(), [fetchUser]);
+	// get user auth
+	useEffect(() => {
+		async function fetchUserHandler() {
+			fetchUser();
+		}
+		fetchUserHandler();
+	}, [fetchUser]);
 
-  // get user auth
-  useEffect(() => {
-    fetchUserHandler();
-  }, [fetchUserHandler]);
-
-  return (
-    <div className={classes.layout}>
-      <Suspense fallback={<Spinner />}>
-        <BrowserRouter>
-          <ActiveLandingLinkProvider>
-            <Header />
-          </ActiveLandingLinkProvider>
-          <main className={classes.main}>
-            <Switch>
-              <Route exact path="/">
-                <ActiveLandingLinkProvider>
-                  <Landing />
-                </ActiveLandingLinkProvider>
-              </Route>
-              <Route exact path="/sign-up" component={SignUp} />
-              <Route exact path="/log-in" component={LogIn} />
-              {/* Employer Routes */}
-              <Route exact path="/job-postings" component={Dashboard} />
-              <Route path="/job-postings/new" component={JobPostNew} />
-              <Route
-                exact
-                path="/job-postings/update"
-                render={props => <JobPostUpdate {...props} />}
-              />
-              <Route
-                exact
-                path="/job-postings/applicants"
-                component={ApplicantsList}
-              />
-              <Route
-                exact
-                path="/job-postings/applicants/:id"
-                component={ApplicantsReview}
-              />
-              {/* Job Seeker Routes */}
-              <Route exact path="/my-resumes" component={JobSeekerDashboard} />
-              <Route path="/resumes/new" component={ResumeNew} />
-              <Route
-                exact
-                path="/resumes/update"
-                render={props => <ResumeUpdate {...props} />}
-              />
-              <Route
-                exact
-                path="/jobs/emailresponse/:choice"
-                component={EmailClickResponse}
-              />
-            </Switch>
-          </main>
-          <ActiveLandingLinkProvider>
-            <Footer style={classes.footer} />
-          </ActiveLandingLinkProvider>
-        </BrowserRouter>
-      </Suspense>
-    </div>
-  );
+	return (
+		<div className={classes.layout}>
+			<Suspense fallback={<Spinner />}>
+				<BrowserRouter>
+					<ActiveLandingLinkProvider>
+						<Header />
+					</ActiveLandingLinkProvider>
+					<main className={classes.main}>
+						<Switch>
+							<Route exact path="/">
+								<ActiveLandingLinkProvider>
+									<Landing />
+								</ActiveLandingLinkProvider>
+							</Route>
+							<Route exact path="/sign-up" component={SignUp} />
+							<Route exact path="/log-in" component={LogIn} />
+							{/* Employer Routes */}
+							<Route exact path="/job-postings" component={Dashboard} />
+							<Route path="/job-postings/new" component={JobPostNew} />
+							<Route
+								exact
+								path="/job-postings/update"
+								render={props => <JobPostUpdate {...props} />}
+							/>
+							<Route
+								exact
+								path="/job-postings/applicants"
+								component={ApplicantsList}
+							/>
+							<Route
+								exact
+								path="/job-postings/applicants/:id"
+								component={ApplicantsReview}
+							/>
+							{/* Job Seeker Routes */}
+							<Route exact path="/my-resumes" component={JobSeekerDashboard} />
+							<Route path="/resumes/new" component={ResumeNew} />
+							<Route
+								exact
+								path="/resumes/update"
+								render={props => <ResumeUpdate {...props} />}
+							/>
+							<Route
+								exact
+								path="/jobs/emailresponse/:choice"
+								component={EmailClickResponse}
+							/>
+						</Switch>
+					</main>
+					<ActiveLandingLinkProvider>
+						<Footer style={classes.footer} />
+					</ActiveLandingLinkProvider>
+				</BrowserRouter>
+			</Suspense>
+		</div>
+	);
 };
 
 function mapStateToProps({ auth }) {
-  return {
-    auth
-  };
+	return {
+		auth
+	};
 }
 
-export default connect(
-  mapStateToProps,
-  { fetchUser }
-)(App);
+export default connect(mapStateToProps, { fetchUser })(App);
