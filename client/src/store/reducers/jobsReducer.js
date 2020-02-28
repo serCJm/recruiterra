7eb7d56aa70afaae51f446db65146afb1bf704df
cjmoro <scjmoro@gmail.com>
2020-02-28 00:06:25 -0800

    Change default loading state to true

--- client/src/store/reducers/jobsReducer.js
+++ client/src/store/reducers/jobsReducer.js
@@ -1,38 +1,38 @@
 import {
-  FETCH_JOBS_START,
-  FETCH_JOBS,
-  DELETE_JOB,
-  FETCH_APPLICANTS
+	FETCH_JOBS_START,
+	FETCH_JOBS,
+	DELETE_JOB,
+	FETCH_APPLICANTS
 } from "../actions/types";
 
 const initialState = {
-  jobsList: [],
-  applicantsList: [],
-  loading: false
+	jobsList: [],
+	applicantsList: [],
+	loading: true
 };
 
 export default function(state = initialState, action) {
-  switch (action.type) {
-    case FETCH_JOBS:
-      return { ...state, jobsList: action.payload, loading: false };
-    case FETCH_JOBS_START:
-      return {
-        ...state,
-        loading: true
-      };
-    case DELETE_JOB:
-      return {
-        ...state,
-        jobsList: action.payload,
-        loading: false
-      };
-    case FETCH_APPLICANTS:
-      return {
-        ...state,
-        applicantsList: action.payload,
-        loading: false
-      };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case FETCH_JOBS:
+			return { ...state, jobsList: action.payload, loading: false };
+		case FETCH_JOBS_START:
+			return {
+				...state,
+				loading: true
+			};
+		case DELETE_JOB:
+			return {
+				...state,
+				jobsList: action.payload,
+				loading: false
+			};
+		case FETCH_APPLICANTS:
+			return {
+				...state,
+				applicantsList: action.payload,
+				loading: false
+			};
+		default:
+			return state;
+	}
 }
--- client/src/store/reducers/resumesReducer.js
+++ client/src/store/reducers/resumesReducer.js
@@ -1,27 +1,27 @@
 import {
-  FETCH_RESUMES,
-  FETCH_RESUMES_START,
-  DELETE_RESUMES
+	FETCH_RESUMES,
+	FETCH_RESUMES_START,
+	DELETE_RESUMES
 } from "../actions/types";
 
 const initialState = {
-  resumesList: [],
-  loading: false
+	resumesList: [],
+	loading: true
 };
 
 export default function resumesReducer(state = initialState, action) {
-  switch (action.type) {
-    case FETCH_RESUMES_START:
-      return { ...state, loading: true };
-    case FETCH_RESUMES:
-      return { ...state, resumesList: action.payload, loading: false };
-    case DELETE_RESUMES:
-      return {
-        ...state,
-        resumesList: action.payload,
-        loading: false
-      };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case FETCH_RESUMES_START:
+			return { ...state, loading: true };
+		case FETCH_RESUMES:
+			return { ...state, resumesList: action.payload, loading: false };
+		case DELETE_RESUMES:
+			return {
+				...state,
+				resumesList: action.payload,
+				loading: false
+			};
+		default:
+			return state;
+	}
 }
