69f0a23f3d2465b4257f8c86310393e18f012d59
cjmoro <scjmoro@gmail.com>
2020-03-29 18:17:59 -0700

    Update mongoose options

--- index.js
+++ index.js
@@ -7,11 +7,14 @@ const keys = require("./config/keys");
 const helmet = require("helmet");
 const compression = require("compression");
 
-mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
+mongoose.connect(keys.mongoURI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
 mongoose.set("useCreateIndex", true);
 // DEV DB DEBUG
 if (process.env.NODE_ENV === "production") {
-  mongoose.set("debug", true);
+	mongoose.set("debug", true);
 }
 require("./models/User");
 require("./models/Job");
@@ -25,15 +28,15 @@ app.use(helmet());
 // NOTE: remember to remove secure and change domain for development
 app.use(bodyParser.json());
 app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey],
-    secure: process.env.NODE_ENV === "production" ? true : false,
-    domain:
-      process.env.NODE_ENV === "production"
-        ? "https://protected-escarpment-75476.herokuapp.com"
-        : null
-  })
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [keys.cookieKey],
+		secure: process.env.NODE_ENV === "production" ? true : false,
+		domain:
+			process.env.NODE_ENV === "production"
+				? "https://protected-escarpment-75476.herokuapp.com"
+				: null
+	})
 );
 app.use(passport.initialize());
 app.use(passport.session());
@@ -44,12 +47,12 @@ require("./routes/employerRoutes")(app);
 require("./routes/jobSeekerRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+	app.use(express.static("client/build"));
 
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+	const path = require("path");
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
 }
 
 const PORT = process.env.PORT || 5000;
