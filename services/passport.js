25a425225c2207f989e3af68cdc6b7277c67fe17
cjmoro <scjmoro@gmail.com>
2020-03-30 00:39:13 -0700

    Update argument

--- services/passport.js
+++ services/passport.js
@@ -6,43 +6,43 @@ const keys = require("../config/keys");
 const User = mongoose.model("users");
 
 passport.serializeUser((user, done) => {
-  done(null, user.id);
+	done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-  User.findById(id).then(user => done(null, user));
+	User.findById({ _id: id }).then(user => done(null, user));
 });
 
 passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback",
-      proxy: true,
-      passReqToCallback: true
-    },
-    async (req, accessToken, refreshToken, profile, done) => {
-      const existingUser = await User.findOne({ googleId: profile.id });
-      if (existingUser) {
-        // user exists, skip
-        return done(null, existingUser);
-      }
-      // no user, create new one
-      let user;
-      try {
-        user = await new User({
-          googleId: profile.id,
-          usertype: req.query.state
-        }).save();
-      } catch (error) {
-        return done(error);
-      }
-      if (user) {
-        return done(null, user);
-      } else {
-        return done(null, null);
-      }
-    }
-  )
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: "/auth/google/callback",
+			proxy: true,
+			passReqToCallback: true
+		},
+		async (req, accessToken, refreshToken, profile, done) => {
+			const existingUser = await User.findOne({ googleId: profile.id });
+			if (existingUser) {
+				// user exists, skip
+				return done(null, existingUser);
+			}
+			// no user, create new one
+			let user;
+			try {
+				user = await new User({
+					googleId: profile.id,
+					usertype: req.query.state
+				}).save();
+			} catch (error) {
+				return done(error);
+			}
+			if (user) {
+				return done(null, user);
+			} else {
+				return done(null, null);
+			}
+		}
+	)
 );
